e8ee3744c047078265cbd2cf7c7100015eb0564f
Revolt <revolt@localhost.localdomain>
2022-02-01 15:53:38 +0100

    hasPath problem

--- script.js
+++ script.js
@@ -1,60 +1,57 @@
 "use strict";
 
-// NOTE: depth-First-Search is implemented using a stack
+// Solving Graph problems
 
-// implementing depth-First-Search using an iterative approach
-const depthFirstSearchIteratively = (graph, sourceNode) => {
-  const stackData = [sourceNode];
+// Solution: using depth-first-search
+const hasPath1 = (graph, src, dst) => {
+  if (src === dst) return true;
 
-  while (stackData.length > 0) {
-    // get current Node from stack
-    const currentNode = stackData.pop();
-    console.log(currentNode);
+  if (!graph[src]) return false;
 
-    // get neighbour nodes of current node
-    for (let neighbour of graph[currentNode]) {
-      // add neighbours to the stack
-      stackData.push(neighbour);
+  for (let neighbour of graph[src]) {
+    if (hasPath(graph, neighbour, dst) === true) {
+      return true;
     }
   }
+  return false;
 };
 
-// implementing depth-First-Search using a recursive approach
-const depthFirstSearchRecursively = (graph, sourceNode) => {
-  console.log(sourceNode);
-
-  for (let currentNode of graph[sourceNode]) {
-    depthFirstSearchRecursively(graph, currentNode);
-  }
-};
-
-// NOTE: bredth-first-search is implemented using a queue
-const bredthFirstSearchRecursively = (graph, sourceNode) => {
-  // add source node to the queue
-  const queueData = [sourceNode];
+// Solution: using bredth-first-search
+const hasPath = (graph, src, dst) => {
+  const queueData = [src];
 
   while (queueData.length > 0) {
-    // get current Node from stack
     const currentNode = queueData.shift();
-    console.log(currentNode);
 
-    // get neighbour nodes of current node
+    if (currentNode === dst) return true;
+
     for (let neighbour of graph[currentNode]) {
-      // add neighbours to the queue
       queueData.push(neighbour);
     }
   }
+
+  return false;
 };
 
-const graph = {
-  a: ["b", "c"],
-  b: ["d"],
-  c: ["e"],
-  d: ["f"],
-  e: [],
-  f: [],
+const graph1 = {
+  f: ["g", "i"],
+  g: ["h"],
+  h: [],
+  i: ["g", "k"],
+  j: ["i"],
+  k: [],
 };
 
-//depthFirstSearchIteratively(graph, "a");
-//depthFirstSearchRecursively(graph, "a");
-bredthFirstSearchRecursively(graph, "a");
+const graph2 = {
+  v: ["x", "w"],
+  w: [],
+  x: [],
+  y: ["z"],
+  z: [],
+};
+
+console.log("test case0 (src: f, dst: k) -> " + hasPath(graph1, "f", "k"));
+console.log("test case0 (src: f, dst: j) -> " + hasPath(graph1, "f", "j"));
+console.log("test case1 (src: i, dst: h) -> " + hasPath(graph1, "i", "h"));
+console.log("test case2 (src: v, dst: w) -> " + hasPath(graph2, "v", "w"));
+console.log("test case3 (src: v, dst: z) -> " + hasPath(graph2, "v", "z"));
