e3792a9b4c232800de6199bf11db4896444fd410
oddy-bassey <oddybassey@gmail.com>
2022-05-15 21:49:27 +0100

    shortest path

--- script.js
+++ script.js
@@ -1,80 +1,52 @@
 "use strict";
 
-// Solving Graph problems (number of connected Components || individual components in a given graph)
+// graph problem (shortest path)
 
-const connectedComponentsCount = (graph) => {
-  const visitedNodes = new Set();
-  let count = 0;
-  for (let node in graph) {
+const shortestPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges);
+  const visited = new Set([nodeA]);
+  const queue = [ [nodeA , 0] ];
 
-    if (explore(graph, node, visitedNodes) == true){
-      count += 1;
+  console.log(graph);
+
+  while(queue.length > 0){
+    const [node, distance] = queue.shift(); 
+
+    if (node === nodeB) return distance;
+
+    for (let neighbour of graph[node]){
+      if(!visited.has(neighbour)){
+        visited.add(neighbour);
+        queue.push([neighbour, distance+1]);
+      }
     }
   }
 
-  return count;
-};
+  return -1;
+}
 
-const explore = (graph, node, visitedNodes) => {
-  if (visitedNodes.has(Number(node))) return 0;
+const buildGraph = (edges) => {
+  const graph = {};
 
-  visitedNodes.add(Number(node));
+  for (let edge of edges) {
+    const [a , b] = edge;
+    
+    if(!(a in graph)) graph[a] = [];
+    if(!(b in graph)) graph[b] = [];
 
-  for (let neighbour of graph[node]) {
-    explore(graph, neighbour, visitedNodes);
+    graph[a].push(b);
+    graph[b].push(a);
   }
 
-  return true;
-};
+  return graph;
+}
 
-// test_00:
-const graph1 = {
-  0: ["8", "1", "5"],
-  1: ["0"],
-  5: ["0", "8"],
-  8: ["0", "5"],
-  2: ["3", "4"],
-  3: ["2", "4"],
-  4: ["3", "2"],
-};
-console.log(connectedComponentsCount(graph1)); // -> 4
+const edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+]
 
-// test_01:
-const graph2 = {
-  1: ["2"],
-  2: ["1", "8"],
-  6: ["7"],
-  9: ["8"],
-  7: ["6", "8"],
-  8: ["9", "7", "2"],
-};
-console.log(connectedComponentsCount(graph2)); // -> 6
-
-// test_02:
-const graph3 = {
-  3: [],
-  4: ["6"],
-  6: ["4", "5", "7", "8"],
-  8: ["6"],
-  7: ["6"],
-  5: ["6"],
-  1: ["2"],
-  2: ["1"],
-};
-console.log(connectedComponentsCount(graph3)); // -> 5
-
-// test_03://
-console.log(connectedComponentsCount({})); // -> 0
-
-// test_04:
-const graph4 = {
-  0: ["4", "7"],
-  1: [],
-  2: [],
-  3: ["6"],
-  4: ["0"],
-  6: ["3"],
-  7: ["0"],
-  8: [],
-};
-console.log(connectedComponentsCount(graph4)); // -> 3
+console.log(shortestPath(edges, 'w', 'z'));
